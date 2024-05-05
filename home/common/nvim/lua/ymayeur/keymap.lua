d349ca7df5e38411b1c9e4ea5aba8208776b83ac
Yannick Mayeur <ymayeur@sweep.net>
2024-05-05 19:30:01 +0200

    feat(vim): binds for quickfix / location list

--- home/common/nvim/lua/ymayeur/keymap.lua
+++ home/common/nvim/lua/ymayeur/keymap.lua
@@ -25,3 +25,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-S-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-S-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
