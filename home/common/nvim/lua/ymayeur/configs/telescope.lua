1bc0d9b7ee2c086f745a0b823dcd2050fc008f43
Yannick Mayeur <ymayeur@sweep.net>
2024-05-30 14:59:47 +0200

    feat(vim): telescope shortcut for TODO-YM

--- home/common/nvim/lua/ymayeur/configs/telescope.lua
+++ home/common/nvim/lua/ymayeur/configs/telescope.lua
@@ -34,3 +34,7 @@ end)
 vim.keymap.set('n', '<leader>fs', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+vim.keymap.set('n', '<leader>fts', function()
+  builtin.grep_string({ search = "TODO-YM" })
+end)
